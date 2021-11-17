2e64ed5ca77f7742ef3a9aca05a242326851e8bd
Stephen Scholtz <scholtz.gnome@gmail.com>
2021-11-17 13:19:22 +0200

    Add validatable enum type and model tests

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,11 @@
 class Artist < ApplicationRecord
+  include ValidatableEnum
+
+  validates :name, presence: { message: 'Name cannot be empty' }, length: { maximum: 100, message: 'Name cannot exceed 100 characters' }
+
+  enum category: %w[band duo singer]
+  validatable_enum :category
+  validates :category, inclusion: { in: categories.keys, message: "%{value} is not a valid category. Category must be one of #{categories.keys}" }
+  
+  validates :debut_year, presence: { message: 'Debut year cannot be empty' }
 end
--- test/models/artist_test.rb
+++ test/models/artist_test.rb
@@ -1,7 +1,38 @@
 require "test_helper"
 
 class ArtistTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :artists
+  
+  test 'new artist attributes cannot be empty' do
+    artist = Artist.new
+
+    assert(artist.invalid?)
+
+    assert(artist.errors[:name].any?)
+    assert_equal(["Name cannot be empty"], artist.errors[:name])
+    
+    assert(artist.errors[:category].any?)
+    
+    assert(artist.errors[:debut_year].any?)
+    assert_equal(["Debut year cannot be empty"], artist.errors[:debut_year])
+  end
+
+  test 'artist category must be either band, duo or singer' do
+    artist = Artist.new(name: artists(:one).name, category: "orchestra", debut_year: artists(:one).debut_year)
+    
+    assert(artist.invalid?)
+
+    assert(artist.errors[:category].any?)
+    assert_equal(['orchestra is not a valid category. Category must be one of ["band", "duo", "singer"]'], artist.errors[:category])
+  end
+
+  test 'artist name must not exceed 100 characters' do
+    long_name = (0..101).map { ('a'..'z').to_a[rand(26)] }.join
+    artist = Artist.new(name: long_name, category: artists(:one).category, debut_year: artists(:one).debut_year)
+    
+    assert(artist.invalid?)
+
+    assert(artist.errors[:name].any?)
+    assert_equal(['Name cannot exceed 100 characters'], artist.errors[:name])
+  end
 end
