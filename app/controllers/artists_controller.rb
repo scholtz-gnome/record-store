d335fc51bf7bd71a593cc2ec7c7f61f92f56a8ed
Stephen Scholtz <scholtz.gnome@gmail.com>
2021-11-18 17:34:58 +0200

    WIP: New artist form

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,9 @@
 class ArtistsController < ApplicationController
+  def index
+    @artists = Artist.all()
+  end
+
+  def new
+    @artist = Artist.new()
+  end
 end
--- test/controllers/artists_controller_test.rb
+++ test/controllers/artists_controller_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class ArtistsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should get index' do
+    get(artists_url)
+    assert_response(:success)
+  end
+
+  test 'should get new' do
+    get(new_artist_url)
+    assert_response(:success)
+  end
 end
