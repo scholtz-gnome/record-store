9450829e05295907f95feeb58f0b00d7028d350c
Stephen Scholtz <scholtz.gnome@gmail.com>
2021-11-22 11:45:19 +0200

    Complete create artist page

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -6,4 +6,19 @@ class ArtistsController < ApplicationController
   def new
     @artist = Artist.new()
   end
+
+  def create
+    @artist = Artist.new(artist_params)
+
+    if @artist.save
+      redirect_to artist_path(@artist)
+    else
+      render :new
+    end
+  end
+
+  private
+    def artist_params
+      params.require(:artist).permit(:name, :category, :debut_year)
+    end
 end
--- test/controllers/artists_controller_test.rb
+++ test/controllers/artists_controller_test.rb
@@ -1,6 +1,10 @@
 require "test_helper"
 
 class ArtistsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @artist = artists(:one)
+  end
+
   test 'should get index' do
     get(artists_url)
     assert_response(:success)
@@ -9,5 +13,22 @@ class ArtistsControllerTest < ActionDispatch::IntegrationTest
   test 'should get new' do
     get(new_artist_url)
     assert_response(:success)
+  end
+
+  test 'should create artst' do
+    assert_difference('Artist.count') do
+      post(
+        artists_url,
+        params: {
+          artist: {
+            name: @artist.name,
+            category: @artist.category,
+            debut_year: @artist.debut_year,
+          }
+        }
+      )
+    end
+
+    assert_redirected_to(artist_url(Artist.last))
   end
 end
